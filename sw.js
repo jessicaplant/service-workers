9f1b5ac7ba3b4b08e8049305b8c04254b341f21a
Jessica Plant <jessica@jessica-plant.co.uk>
2017-09-07 20:25:50 +0100

    respond with cached items

--- sw.js
+++ sw.js
@@ -17,3 +17,6 @@ self.addEventListener('install', function(event) {
             })
     );
 });
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request));
+});
