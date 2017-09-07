eb5a5e6b627e03dea9751ebfaf7d35f6676948b1
Jessica Plant <jessica@jessica-plant.co.uk>
2017-09-07 19:47:38 +0100

    added in app.js correctly

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jessi on 07/09/2017.
+ */
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
