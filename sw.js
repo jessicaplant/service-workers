9b53dc065a9efd424b91cc258a4690d6e071c02e
Jessica Plant <jessica@jessica-plant.co.uk>
2017-09-07 20:17:11 +0100

    im an idiot

--- sw.js
+++ sw.js
@@ -1,21 +1,19 @@
 /**
  * Created by jessi on 07/09/2017.
  */
-self.addEventListener('install', function(event) {
-    var CACHE_NAME = 'my-site-cache-v1';
-    var urlsToCache = [
-        '/',
-        '/img/pic.png'
-    ];
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/',
+    '/img/pic.png'
+];
 
-    self.addEventListener('install', function(event) {
-        // Perform install steps
-        event.waitUntil(
-            caches.open(CACHE_NAME)
-                .then(function(cache) {
-                    console.log('Opened cache');
-                    return cache.addAll(urlsToCache);
-                })
-        );
-    });
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
