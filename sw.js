21c164e5485033e7cafbaecaa9a93577fed98b78
Jessica Plant <jessica@jessica-plant.co.uk>
2017-09-07 20:09:44 +0100

    added in app.js correctly

--- sw.js
+++ sw.js
@@ -2,5 +2,20 @@
  * Created by jessi on 07/09/2017.
  */
 self.addEventListener('install', function(event) {
-    // Perform install steps
+    var CACHE_NAME = 'my-site-cache-v1';
+    var urlsToCache = [
+        '/',
+        '/img'
+    ];
+
+    self.addEventListener('install', function(event) {
+        // Perform install steps
+        event.waitUntil(
+            caches.open(CACHE_NAME)
+                .then(function(cache) {
+                    console.log('Opened cache');
+                    return cache.addAll(urlsToCache);
+                })
+        );
+    });
 });
